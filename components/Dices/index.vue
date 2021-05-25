901d63bf31f022b58d3ef94e657edb16e9b49a7e
nhat thi <dinhnhatthi1996@gmail.com>
2021-05-25 21:17:47 +0700

    ad image

--- components/Dices/index.vue
+++ components/Dices/index.vue
@@ -12,7 +12,9 @@
     </div>
     <div id="dice-2" class="dice">
       <div :class="'spinner dice-' + dices[1]">
-        <div class="face1">1</div>
+        <div class="face1">
+          <img src="/images/dice-1.png" />
+        </div>
         <div class="face2">2</div>
         <div class="face3">3</div>
         <div class="face4">4</div>
